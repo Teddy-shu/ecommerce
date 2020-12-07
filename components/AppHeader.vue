da940a8cc4af8af1fd2b8b55d3fcbb567ea62045
Teddy SHU <wne333wne@gmail.com>
2020-12-07 23:07:20 +0800

    first commit

---
+++ components/AppHeader.vue
@@ -0,0 +1,56 @@
+<template lang="html">
+  <div>
+    <v-card class="overflow-hidden">
+      <v-app-bar absolute color="#fff" elevate-on-scroll scroll-target="#scrolling-techniques-7">
+        <!--
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Title</v-toolbar-title>
+        -->
+        <img src="~/assets/img/logo.png" class="my-mr" />
+
+        <v-hover v-slot="{ hover } " v-for="(link, index) in linkTitles" :key="index">
+          <v-btn text
+           class="text-uppercase"
+           :color="hover ? '#6f42c1' : 'black'"
+          >
+            {{ link }}
+          </v-btn>
+        </v-hover>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-btn depressed dark rounded color="#9F78FF">
+          <v-icon class="pr-2">mdi-shopping</v-icon>
+          <p>{{ chartCount }}</p>
+        </v-btn>
+
+      </v-app-bar>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    linkTitles: ['home', 'shop', 'about', 'contact'],
+    chartCount: 0
+  })
+}
+</script>
+
+<style lang="css" scoped>
+.v-btn::before {
+  background-color: transparent;
+}
+.my-mr {
+  margin-right: 32px;
+}
+.pr-2 {
+  padding-right: 4px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="container">
+    <div class="absolute-spacing"></div>
+    <v-card>
+      <v-carousel :show-arrows="false"
+      width="90vww"
+      height="fit-content"
+      cycle
+      >
+       <v-carousel-item
+        width="90vww"
+        height=""
+        v-for="(item,i) in carouselItems"
+        :key="i"
+       >
+       <img :src="item" class="carousel-img" />
+     </v-carousel-item>
+     </v-carousel>
+     <div class="text-zone">
+       <h1>fashion</h1>
+       <h1>changing</h1>
+       <h1>always</h1>
+       <v-btn depressed dark x-large color="#9F78FF" class="rounded-pill">
+         <p>Shop Now</p>
+       </v-btn>
+     </div>
+    </v-card>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    carouselItems: [
+      '/img/arnel-hasanovic-Nl-SXO4FAHw-unsplash.jpg',
+      '/img/annie-spratt-CQ2YvMAN7zE-unsplash.jpg',
+      '/img/erin-east-14YmL-R6MCc-unsplash.jpg'
+    ]
+  })
+}
+</script>
+
+<style>
+.absolute-spacing {
+  width: 100vw;
+  height:64px;
+}
+.carousel-img {
+  max-width:100%;
+  height:auto;
+}
+.text-zone {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  max-width: 50vw;
+  max-height: 50vh;
+}
+.text-zone h1 {
+  font-size: 8vw;
+  font-weight: 700;
+  color: rgba(255,255,255,0.87);
+  line-height: 8vw;
+  margin-bottom: calc(8vw / 10);
+  font-family: "Cinzel",serif;
+  text-transform: uppercase;
+}
+.text-zone p {
+  font-size: 1.25rem;
+  line-height: 1.25rem;
+}
+
+</style>
